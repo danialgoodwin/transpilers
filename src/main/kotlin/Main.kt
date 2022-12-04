0874357ea137fd700b407e3a5807409477b0a5ef
Danial Goodwin <danialgoodwin@gmail.com>
2022-12-03 19:03:31 -0700

    Add tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,18 @@ fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
 
-    val input = "(add 2 (sub 4 3))"
-    val expectedOutput = "add(2, sub(4, 3))"
+    val input = "(add 1 2)"
+    val expectedOutput = "add(1, 2);"
+    val output = LispToJavascriptTranspiler().transpile(input)
+    assert(output == expectedOutput)
 
-    LispToJavascriptTranspiler().transpile(input)
+    val input1 = "(add 2 (sub 4 3))"
+    val expectedOutput1 = "add(2, sub(4, 3));"
+    val output1 = LispToJavascriptTranspiler().transpile(input1)
+    assert(output1 == expectedOutput1)
+
+    val input2 = "(add 2 (sub (add 1 2) (add 3 4)))"
+    val expectedOutput2 = "add(2, sub(add(1, 2), add(3, 4)));"
+    val output2 = LispToJavascriptTranspiler().transpile(input2)
+    assert(output2 == expectedOutput2)
 }
